375d9d7374ae3ef71953ccdf1968fff96063774f
abee29 <nazarethh112@gmail.com>
2025-11-17 19:57:39 -0500

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+console.log("Luxe Loop Loaded");
+
+// Open / close cart panel
+document.getElementById("cartToggle").onclick = () => {
+  document.getElementById("cartPanel").classList.toggle("open");
+};
